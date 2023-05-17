ecac968bd5063c8d9e7c80ddeab94a3078a1cf59
RaXianch <1025212779@qq.com>
2023-05-17 17:29:23 +0800

    fronedn rename

--- backEnd/server_core/server.py
+++ backEnd/server_core/server.py
@@ -12,7 +12,8 @@ from starlette.responses import RedirectResponse
 # 导入项目view
 # from apps import user,test
 # from apps.user import user, menu
-from apps import test
+# from apps import test
+from apps.route import *
 from server_core.middleware import register_cors, register_exception
 from server_core.conf import conf
 
@@ -33,6 +34,10 @@ def createapp():
     # @app.get("/")
     # def home():
     #     return RedirectResponse(url="/otdisk/index.html")
+
+    @app.get("/test")
+    def test():
+        return {"holy": "shit！"}
 
     register_cors(app)  # 跨域设置
     # register_exception(app)
