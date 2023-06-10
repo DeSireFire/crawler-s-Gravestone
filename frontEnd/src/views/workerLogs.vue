4df6c441f658c30403425cdbc0ccd31a0b44ac96
RaXianch <1025212779@qq.com>
2023-06-10 13:47:27 +0800

    调整部署到服务器上的端口设置

---
+++ frontEnd/src/views/workerLogs.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="handle-box">
+        <el-select v-model="query.log_project" placeholder="项目名称" class="handle-select mr10">
+          <el-option key="1" label="高德地图" value="高德地图"></el-option>
+          <el-option key="2" label="美团药店" value="美团药店"></el-option>
+          <el-option key="2" label="企查查" value="企查查"></el-option>
+        </el-select>
+        <el-input v-model="query.name" placeholder="搜索词" class="handle-input mr10"></el-input>
+        <el-button type="primary" :icon="Search" @click="handleSearch">搜索/刷新</el-button>
+        <el-button type="primary" :icon="Plus">新增</el-button>
+      </div>
+      <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+        <el-table-column prop="log_project" label="所属项目"></el-table-column>
+        <el-table-column prop="name" label="日志名称"></el-table-column>
+        <el-table-column label="日志备注">
+          <template #default="scope">{{ scope.row.remarks }}</template>
+        </el-table-column>
+        <el-table-column prop="address" label="来源ip"></el-table-column>
+        <el-table-column label="操作" width="220" align="center">
+          <template #default="scope">
+            <el-button text :icon="Edit" @click="handleEdit(scope.$index, scope.row)" v-permiss="15">
+              查看
+            </el-button>
+            <el-button text :icon="Delete" class="red" @click="handleDelete(scope.$index)" v-permiss="16">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pagination">
+        <el-pagination
+            background
+            layout="total, prev, pager, next"
+            :current-page="query.pageIndex"
+            :page-size="query.pageSize"
+            :total="pageTotal"
+            @current-change="handlePageChange"
+        ></el-pagination>
+      </div>
+    </div>
+
+    <!-- 编辑弹出框 -->
+    <el-dialog title="编辑" v-model="editVisible" width="30%">
+      <el-form label-width="70px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+				<span class="dialog-footer">
+					<el-button @click="editVisible = false">取 消</el-button>
+					<el-button type="primary" @click="saveEdit">确 定</el-button>
+				</span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup lang="ts" name="basetable">
+import { ref, reactive } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { Delete, Edit, Search, Plus } from '@element-plus/icons-vue';
+import { getLogs } from '../api/workerLogs';
+
+interface TableItem {
+  id: number;
+  log_project: string;
+  name: string;
+  remarks: string;
+  address: string;
+}
+// localStorage.setItem('key', JSON.stringify(data));
+// const data = JSON.parse(localStorage.getItem('key'));
+
+const query = reactive({
+  address: '',
+  name: '',
+  pageIndex: 1,
+  pageSize: 10
+});
+const tableData = ref<TableItem[]>([]);
+const pageTotal = ref(0);
+// 获取表格数据
+const getData = () => {
+  getLogs().then(res => {
+    // console.log("res.data", res.data)
+    tableData.value = res.data.list;
+    pageTotal.value = res.data.pageTotal || 1;
+    localStorage.setItem('workerLogs', JSON.stringify(res.data));
+  });
+};
+getData();
+
+
+// 查询操作
+const handleSearch = () => {
+  query.pageIndex = 1;
+  getData();
+};
+// 分页导航
+const handlePageChange = (val: number) => {
+  query.pageIndex = val;
+  let list_index = (val-1)*10-1
+  const data = JSON.parse(localStorage.getItem('workerLogs'));
+  console.log("workerLogs", data.list.slice(list_index))
+  tableData.value = data.list.slice(list_index);
+  this.$refs.tableData.doLayout();
+};
+
+// 删除操作
+const handleDelete = (index: number) => {
+  // 二次确认删除
+  ElMessageBox.confirm('确定要删除吗？', '提示', {
+    type: 'warning'
+  })
+      .then(() => {
+        ElMessage.success('删除成功');
+        tableData.value.splice(index, 1);
+      })
+      .catch(() => {});
+};
+
+// 表格编辑时弹窗和保存
+const editVisible = ref(false);
+let form = reactive({
+  name: '',
+  address: ''
+});
+let idx: number = -1;
+const handleEdit = (index: number, row: any) => {
+  idx = index;
+  form.name = row.name;
+  form.address = row.address;
+  editVisible.value = true;
+};
+const saveEdit = () => {
+  editVisible.value = false;
+  ElMessage.success(`修改第 ${idx + 1} 行成功`);
+  tableData.value[idx].name = form.name;
+  tableData.value[idx].address = form.address;
+};
+</script>
+
+<style scoped>
+.handle-box {
+  margin-bottom: 20px;
+}
+
+.handle-select {
+  width: 120px;
+}
+
+.handle-input {
+  width: 300px;
+}
+.table {
+  width: 100%;
+  font-size: 14px;
+}
+.red {
+  color: #F56C6C;
+}
+.mr10 {
+  margin-right: 10px;
+}
+.table-td-thumb {
+  display: block;
+  margin: auto;
+  width: 40px;
+  height: 40px;
+}
+</style>
