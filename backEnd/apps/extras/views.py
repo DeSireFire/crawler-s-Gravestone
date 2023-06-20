2e5fabfdb1d126076328cbbde935f0e1a5b89e80
RaXianch <1025212779@qq.com>
2023-06-20 11:35:05 +0800

    删模拟删除代码

--- backEnd/apps/extras/views.py
+++ backEnd/apps/extras/views.py
@@ -36,28 +36,28 @@ async def ipInfo():
 #     return callbackJson.callBacker(cache_datas)
 
 
-@route.delete("/del_logs")  # todo 属于危险操作需要鉴权
-async def del_logs(request: Request, name: str = Query(None)):
-    """
-    接收要删除的日志文件数据
-    参数以url传参的方式接收，数据结构为
-    {'id': '52', 'name': 'ip_89_demo_local.log', 'log_project': '美团药店', 'remarks': '12月-月度采集日志', 'address': 'localhost'}
-    :param request: 请求对象
-    :param name: 请求传输过来的name参数
-    :return:
-    """
-    callbackJson = constructResponse()
-    callbackJson.statusCode = 200
-    callbackJson.url = request.url
-    content = {}
-    cache_list = cache_datas.get("list", [])
-    del_data = dict(request.query_params)
-    del_index = [i for i, x in enumerate(cache_list) if x == del_data]
-    if del_index:
-        content = cache_list.pop(del_index[0])
-        cache_datas["list"] = cache_list
-        cache_datas["pageTotal"] = len(cache_list)
-    else:
-        callbackJson.statusCode = 404
-        callbackJson.message = "服务器找不到请求的资源"
-    return callbackJson.callBacker(content)
+# @route.delete("/del_logs")  # todo 属于危险操作需要鉴权
+# async def del_logs(request: Request, name: str = Query(None)):
+#     """
+#     接收要删除的日志文件数据
+#     参数以url传参的方式接收，数据结构为
+#     {'id': '52', 'name': 'ip_89_demo_local.log', 'log_project': '美团药店', 'remarks': '12月-月度采集日志', 'address': 'localhost'}
+#     :param request: 请求对象
+#     :param name: 请求传输过来的name参数
+#     :return:
+#     """
+#     callbackJson = constructResponse()
+#     callbackJson.statusCode = 200
+#     callbackJson.url = request.url
+#     content = {}
+#     cache_list = cache_datas.get("list", [])
+#     del_data = dict(request.query_params)
+#     del_index = [i for i, x in enumerate(cache_list) if x == del_data]
+#     if del_index:
+#         content = cache_list.pop(del_index[0])
+#         cache_datas["list"] = cache_list
+#         cache_datas["pageTotal"] = len(cache_list)
+#     else:
+#         callbackJson.statusCode = 404
+#         callbackJson.message = "服务器找不到请求的资源"
+#     return callbackJson.callBacker(content)
