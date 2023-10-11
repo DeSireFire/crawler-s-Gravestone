eb65bf484c2e70d2e9f679fd237d905a87c477d5
RaXianch <1025212779@qq.com>
2023-10-11 15:44:14 +0800

    修复：守护程序定时任务类型错误导致的启动失败的问题

--- backEnd/run_guardServe.py
+++ backEnd/run_guardServe.py
@@ -13,12 +13,30 @@ from server_core.conf import conf, LogLevel
 from server_core.hello import logger
 
 if __name__ == '__main__':
+
+    # task_scheduler = TaskScheduler()
+    # # 添加任务
+    # # task_scheduler.add_task("task1", sample_job, "interval", seconds=10)
+    # # 过往任务过期判断
+    # task_scheduler.add_task("update_job_statuses", update_job_statuses, "interval", seconds=600)
+    #
+    # try:
+    #     # 这里可以添加你的其他程序逻辑
+    #     # 例如，可以运行一个无限循环以保持程序运行，或者在这里执行其他任务
+    #     while True:
+    #         print("平台守护程序运行中..")
+    #         time.sleep(600)
+    # except (KeyboardInterrupt, SystemExit):
+    #     # 当你手动停止程序时，由atexit注册的退出处理函数会关闭定时任务调度器
+    #     pass
+
+
     # with 代码块来降低意外崩溃时，子程没正常关闭的情况
     with TaskScheduler() as task_scheduler:
         # 添加任务
         # task_scheduler.add_task("task1", sample_job, "interval", seconds=10)
         # 过往任务过期判断
-        task_scheduler.add_task("update_job_statuses", update_job_statuses, "interval", minute=10)
+        task_scheduler.add_task("update_job_statuses", update_job_statuses, "interval", seconds=600)
 
         try:
             # 这里可以添加你的其他程序逻辑
@@ -28,9 +46,11 @@ if __name__ == '__main__':
                 time.sleep(600)
         except (KeyboardInterrupt, SystemExit):
             # 当你手动停止程序时，由atexit注册的退出处理函数会关闭定时任务调度器
-            print(666)
+            pass
 
         # 清理所有任务
         # print("清理所有任务...")
         # task_scheduler.clear_all_tasks()
+
+
 
